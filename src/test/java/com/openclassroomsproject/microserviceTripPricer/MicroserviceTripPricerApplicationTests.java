b31d7caa828198ebf06650285744d2f13e323159
jonathan <jonathan.developpeur@gmail.com>
2022-06-18 12:32:58 +0200

    create-github-repo

---
+++ src/test/java/com/openclassroomsproject/microserviceTripPricer/MicroserviceTripPricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroomsproject.microserviceTripPricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTripPricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
